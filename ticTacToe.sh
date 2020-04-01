5eb88fdf258043f96baedcc4e0f6a577fa112620
pranav8596 <pige786123@gmail.com>
2020-04-01 20:07:03 +0530

    ADDED: Computer plays with Player by chosing random positions

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -13,19 +13,24 @@ function symbolAssignment() {
 	if [ $((RANDOM%2)) -eq 0 ]
 	then
 		playerSymbol=X
+		computerSymbol=O
 	else
 		playerSymbol=O
+		computerSymbol=X
 	fi
-	echo -e "Symbol Assigned to Player   : $playerSymbol\n"
+	echo "Symbol Assigned to Player   : $playerSymbol"
+	echo "Symbol Assigned to Computer  : $computerSymbol"
 }
 
 #To check who plays First
 function checkWhoPlaysFirst() {
 	if [ $((RANDOM%2)) -eq 0 ]
 	then
-		echo -e "Player plays First\n"
+		echo "Player plays First"
+		switchPlayer=0
 	else
-		echo -e "Computer plays First\n"
+		echo "Computer plays First"
+		switchPlayer=1
 	fi
 }
 
@@ -41,34 +46,38 @@ function displayBoard() {
 
 #To check for all winning conditions
 function checkWinConditions(){
+	symbol=$1
 	#Check for Rows
 	for (( i=1; i<=9; i=$(($i+3 )) ))
 	do
-		if [[ ${gameBoard[$i]} == $playerSymbol ]] && [[ ${gameBoard[$i]} == ${gameBoard[$i+1]} ]] && [[ ${gameBoard[$i+1]} == ${gameBoard[$i+2]} ]]
+		if [[ ${gameBoard[$i]} == $symbol ]] && [[ ${gameBoard[$i]} == ${gameBoard[$i+1]} ]] && [[ ${gameBoard[$i+1]} == ${gameBoard[$i+2]} ]]
 		then
-			echo "Won"
-			exit
+			isWin=1
 		fi
 	done
 
 	#Check for Columns
 	for (( i=1; i<=9; i++ ))
 	do
-		if [[ ${gameBoard[$i]} == $playerSymbol ]] && [[ ${gameBoard[$i]} == ${gameBoard[$i+3]} ]] && [[ ${gameBoard[$i+3]} == ${gameBoard[$i+6]} ]]
+		if [[ ${gameBoard[$i]} == $symbol ]] && [[ ${gameBoard[$i]} == ${gameBoard[$i+3]} ]] && [[ ${gameBoard[$i+3]} == ${gameBoard[$i+6]} ]]
 		then
-			echo "Won"
-			exit
+			isWin=1
 		fi
 	done
 
 	#Check for Diagonals
-	if [[ ${gameBoard[1]} == $playerSymbol ]] && [[ ${gameBoard[1]} == ${gameBoard[5]} ]] && [[ ${gameBoard[5]} == ${gameBoard[9]} ]]
+	if [[ ${gameBoard[1]} == $symbol ]] && [[ ${gameBoard[1]} == ${gameBoard[5]} ]] && [[ ${gameBoard[5]} == ${gameBoard[9]} ]]
 	then
-		echo "Won"
-		exit
-	elif [[ ${gameBoard[3]} == $playerSymbol ]] && [[ ${gameBoard[3]} == ${gameBoard[5]} ]] && [[ ${gameBoard[5]} == ${gameBoard[7]} ]]
+		isWin=1
+	elif [[ ${gameBoard[3]} == $symbol ]] && [[ ${gameBoard[3]} == ${gameBoard[5]} ]] && [[ ${gameBoard[5]} == ${gameBoard[7]} ]]
 	then
-		echo "Won"
+		isWin=1
+	fi
+
+	#To determine who has won
+	if [[ $isWin -eq 1 ]]
+	then
+		echo "$2 Won"
 		exit
 	fi
 }
@@ -84,41 +93,72 @@ function checkTie() {
 
 #To insert symbol at a particular position
 function insertSymbol() {
-	gameBoard[$position]=$playerSymbol
+	if [[ $switchSymbol -eq 1 ]]
+	then
+		gameBoard[$position]=$playerSymbol
+	else
+		gameBoard[$position]=$computerSymbol
+	fi
 	((count++))
 }
 
 #To check if the position is Empty or Valid
-function isEmpty() {
+	function isEmpty() {
+	position=$1
 	if [[ $position -ge 1 ]] && [[ $position -le 9 ]]
 	then
-		if [[ ${gameBoard[$position]} != $playerSymbol ]]
+		if [[ ${gameBoard[$position]} != $playerSymbol ]] &&  [[ ${gameBoard[$position]} != $computerSymbol ]]
 		then
 			insertSymbol
+			displayBoard
 		else
 			echo "This position is not Empty. Enter again."
-	fi
+			switchThePlayers
+		fi
 	else
 		echo "Invalid position!!"
+		switchThePlayers
 	fi
 }
 
-#To start the Game Play
-function startThePlay() {
+function playersTurn() {
+	read -p "Its Player's turn. Enter your move: " playerPosition
+	switchSymbol=1
+	isEmpty $playerPosition $playerSymbol $computerSymbol
+	checkWinConditions $playerSymbol "PLAYER"
+	switchPlayer=1
+}
+
+function computersTurn() {
+	computerPosition=$((RANDOM%9+1))
+	echo "Its Computer's turn. Computer's move:  $computerPosition"
+	switchSymbol=2
+	isEmpty $computerPosition $playerSymbol $computerSymbol
+	checkWinConditions $computerSymbol "COMPUTER"
+	switchPlayer=0
+}
+
+function switchThePlayers() {
+ while [[ $count -ne 9 ]]
+   do
+      if [[ $switchPlayer -eq 0 ]]
+      then
+         playersTurn
+      else
+         computersTurn
+      fi
+   done
+   checkTie
+}
+
+#To start the Tic Toc Toe Game Play
+function ticTacToeGame() {
 	resetTheBoard
 	checkWhoPlaysFirst
 	symbolAssignment
 	displayBoard
-	while [[ $count -ne 9 ]]
-	do
-		checkTie
-		read -p "Enter your position: " position
-		isEmpty
-		displayBoard
-		checkWinConditions
-	done
-	checkTie
+	switchThePlayers
 }
 
 #Main
-startThePlay
+ticTacToeGame
