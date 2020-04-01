42d4676bc29cfc464f359b201f47ecbe00f7bea9
pranav8596 <pige786123@gmail.com>
2020-04-01 13:42:05 +0530

    ADDED: A function to assign symbols to Player and Computer

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 #Declaration of the Game Board Array
 declare -a gameBoard
@@ -6,4 +6,18 @@ declare -a gameBoard
 #To reset the Game Board
 function resetTheBoard() {
 	gameBoard=(0 1 2 3 4 5 6 7 8 9)
+}
+
+#To assign a symbol to Player and Computer
+function symbolAssignment() {
+	if [ $((RANDOM%2)) -eq 0 ]
+	then
+		playerSymbol=X
+		computerSymbol=O	
+	else
+		playerSymbol=O
+		computerSymbol=X
+	fi
+	echo "Symbol Assigned to Player   :" $playerSymbol
+	echo "Symbol Assigned to Computer :" $computerSymbol
 }
